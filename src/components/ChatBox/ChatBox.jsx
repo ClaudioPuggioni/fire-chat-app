0a8d6c43eeb4da6a97daf523930e273742fe7f88
Claudio Puggioni <claudieddu@hotmail.com>
2022-08-11 04:33:39 -0700

    final until not final

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -8,13 +8,16 @@ function ChatBox(props) {
 
   useEffect(() => {
     status.currentUser.email === props.msgInfo.senderEmail ? setUserIsSender(true) : setUserIsSender(false);
+    console.log(props);
   }, []);
 
   //Date().toLocaleString()
   //style={{ left: props.sender === "user" ? "-5px" : "" }}
   return (
     <div className="chatBoxUser" style={{ alignSelf: userIsSender ? "flex-start" : "flex-end" }}>
-      <div className="email">{userIsSender ? props.msgInfo.senderEmail : null}</div>
+      <div className="email" style={{ textAlign: userIsSender ? "left" : "right" }}>
+        {userIsSender ? props.msgInfo.senderEmail : props.msgInfo.sender}
+      </div>
       <div className="date">{props.msgInfo.date}</div>
       <div className="chatBox" style={{ backgroundColor: userIsSender ? "#BADEFC" : "#FFBDC9", alignSelf: userIsSender ? "flex-start" : "flex-end" }}>
         <div className="tri" style={{ left: userIsSender ? "-8px" : "", right: userIsSender ? "" : "-8px", color: userIsSender ? "#BADEFC" : "#FFBDC9" }}>
